8771ccd060be09f59ead5ae4e16ae749575f82bd
Ian Daniel <idaniel@spinnys.com>
2017-01-31 18:59:39 +1000

    Add nomnom.js

---
+++ src/js/nomnom.js
@@ -0,0 +1,30 @@
+function nomnom() {
+  var restURL = localStorage.getItem('restURL');
+
+  var request = $.ajax({
+    method: "GET",
+    url: restURL,
+  })
+    .done(function(res) {
+      updateScoreboard(res.data);
+    })
+    .fail(function( jqXHR, textStatus ) {
+      // console.log('jqXHR');
+      // console.log(jqXHR);
+      // console.log('textStatus');
+      // console.log(textStatus);
+    });
+
+  // console.log('request');
+  // console.log(request);
+
+
+  var eventDispatchLog = $('#eventDispatchLog').val();
+  eventDispatchLog = eventDispatchLog.length === 0 ? randomEvent : eventDispatchLog + "\n" + randomEvent;
+  $('#eventDispatchLog').val(eventDispatchLog);
+
+  var endpointLog = localStorage.getItem('endpointLog');
+  $('#endpointLog').val(endpointLog);
+
+  // console.log('dispatchEvent: ' + randomEvent);
+}
