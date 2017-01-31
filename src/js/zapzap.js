bd4f0c1a1c1a2865be831711353c0200bcb58745
Timothy Marwick <triyuga@gmail.com>
2017-01-31 17:59:09 +1000

    remove some craps

--- src/js/zapzap.js
+++ src/js/zapzap.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
  *
  */
 function doTheZapZap() {
-  // doZapzap!
+  // doTheZapZap!
   var interval = Math.random() * (5000 - 2000) + 2000;
   var zapzapTimer = setInterval(pauseZapzap, interval);
   $("body").addClass("zapazap");
@@ -16,6 +16,7 @@ function doTheZapZap() {
  *
  */
 function waitForTheZapZap() {
+  // waitForTheZapZap!
   var interval = Math.random() * (5000 - 2000) + 2000;
   setInterval(doZapzap, interval);
   $("body").removeClass("zapazap");
