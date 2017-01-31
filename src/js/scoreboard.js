0cdd5e0cd46b52b35f0667e4e80ce65ca1ac8960
Ian Daniel <idaniel@spinnys.com>
2017-01-31 19:25:31 +1000

    updates

--- src/js/scoreboard.js
+++ src/js/scoreboard.js
@@ -2,5 +2,16 @@
 function updateScoreboard(data) {
     console.log('data', data);
 
-    
+    let stats = {
+        killCount: data.killCount || "Zero Kill",
+        spellCount: data.spellCount || "No Spell",
+        leaderBoard: data.leaderBoard || "No Leaders",
+    };
+
+    $('#killCount').append( function() {
+        return stats.killCount.map(function(item){
+            return "<li>" + item + "</li>";
+        });
+    });
+
 }
