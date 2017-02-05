57839cf7ab2ecdfd0cca5ae3ab5a5c1753ff651b
Joshua Wulf <sitapati@worldsankirtan.net>
2017-02-05 18:05:24 +1000

    Add body parser and change to POST

--- server.js
+++ server.js
@@ -1,107 +1,113 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var store = [];
 
 function getStats() {
-  var stats = {
-    killCount: {}, // entityType: int
-    spellCount: {}, // spellName: int
-    leaderBoard: [], // [ { playerName: stats } ]
-  };
+	var stats = {
+		killCount: {}, // entityType: int
+		spellCount: {}, // spellName: int
+		leaderBoard: [], // [ { playerName: stats } ]
+	};
 
-  var playerStats = {};
+	var playerStats = {};
 
-  store.map(function (event) {
-    console.log(event);
-    switch(event.eventType) {
-      case "playerKilledEntity":
-          // killCount
-          var entityType = event.entityType;
-          if (!stats.killCount[entityType]) {
-            stats.killCount[entityType] = 0;
-          }
-          stats.killCount[entityType]++;
+	store.map(function (event) {
+		console.log(event);
+		switch (event.eventType) {
+			case "playerKilledEntity":
+				// killCount
+				var entityType = event.entityType;
+				if (!stats.killCount[entityType]) {
+					stats.killCount[entityType] = 0;
+				}
+				stats.killCount[entityType]++;
 
-          // playerStats
-          var playerName = event.playerName;
-          if (!playerStats[playerName]) {
-          // if (!playerStats[playerName]) {
-            playerStats[playerName] = {
-              spellCount: {},
-              killCount: {},
-              totalSpells: 0,
-              totalKills: 0,
-              totalEvents: 0
-            };
-          }
-          if (!playerStats[playerName].killCount[entityType]) {
-            playerStats[playerName].killCount[entityType] = 0;
-          }
-          playerStats[playerName].killCount[entityType]++;
-          playerStats[playerName].totalKills++;
-          playerStats[playerName].totalEvents++;
-          break;
+				// playerStats
+				var playerName = event.playerName;
+				if (!playerStats[playerName]) {
+					// if (!playerStats[playerName]) {
+					playerStats[playerName] = {
+						spellCount: {},
+						killCount: {},
+						totalSpells: 0,
+						totalKills: 0,
+						totalEvents: 0
+					};
+				}
+				if (!playerStats[playerName].killCount[entityType]) {
+					playerStats[playerName].killCount[entityType] = 0;
+				}
+				playerStats[playerName].killCount[entityType]++;
+				playerStats[playerName].totalKills++;
+				playerStats[playerName].totalEvents++;
+				break;
 
-      case "playerCastSpell":
-          // spellCount
-          var spellName = event.spellName;
-          if (!stats.spellCount[spellName]) {
-            stats.spellCount[spellName] = 0;
-          }
-          stats.spellCount[spellName]++;
+			case "playerCastSpell":
+				// spellCount
+				var spellName = event.spellName;
+				if (!stats.spellCount[spellName]) {
+					stats.spellCount[spellName] = 0;
+				}
+				stats.spellCount[spellName]++;
 
-          // playerStats
-          var playerName = event.playerName;
-          if (!playerStats[playerName]) {
-            playerStats[playerName] = {
-              spellCount: {},
-              killCount: {},
-              totalSpells: 0,
-              totalKills: 0,
-              totalEvents: 0
-            };
-          }
-          if (!playerStats[playerName].spellCount[spellName]) {
-            playerStats[playerName].spellCount[spellName] = 0;
-          }
-          playerStats[playerName].spellCount[spellName]++;
-          playerStats[playerName].totalSpells++;
-          playerStats[playerName].totalEvents++;
-          break;
+				// playerStats
+				var playerName = event.playerName;
+				if (!playerStats[playerName]) {
+					playerStats[playerName] = {
+						spellCount: {},
+						killCount: {},
+						totalSpells: 0,
+						totalKills: 0,
+						totalEvents: 0
+					};
+				}
+				if (!playerStats[playerName].spellCount[spellName]) {
+					playerStats[playerName].spellCount[spellName] = 0;
+				}
+				playerStats[playerName].spellCount[spellName]++;
+				playerStats[playerName].totalSpells++;
+				playerStats[playerName].totalEvents++;
+				break;
 
-    }
-  });
+		}
+	});
 
-  for (var playerName in playerStats) {
-    playerStats[playerName].playerName = playerName;
-    stats.leaderBoard.push(playerStats[playerName]);
-  }
+	for (var playerName in playerStats) {
+		playerStats[playerName].playerName = playerName;
+		stats.leaderBoard.push(playerStats[playerName]);
+	}
 
-  stats.leaderBoard.sort(sortPlayerStats);
+	stats.leaderBoard.sort(sortPlayerStats);
 
-  return stats;
+	return stats;
 }
 
-function sortPlayerStats(a,b) {
-  if (a.totalEvents > b.totalEvents)
-    return -1;
-  if (a.totalEvents < b.totalEvents)
-    return 1;
-  return 0;
+function sortPlayerStats(a, b) {
+	if (a.totalEvents > b.totalEvents)
+		return -1;
+	if (a.totalEvents < b.totalEvents)
+		return 1;
+	return 0;
 }
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  if (req.method === 'OPTIONS') {
-    return res.send(200);
-  } else {
-    return next();
-  }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+	res.header('Access-Control-Expose-Headers', 'Content-Length');
+	res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+	if (req.method === 'OPTIONS') {
+		return res.send(200);
+	} else {
+		return next();
+	}
 });
 
 // app.all('/', function(req, res, next) {
@@ -112,41 +118,63 @@ app.use(function(req, res, next) {
 
 // Consumes data from mock emitter.
 app.get('/eat', function (req, res) {
-  store.push(req.query);
-  console.log('store');
-  console.log(req.query);
-  res.json({
-    msg: 'Nom nom!',
-    query: req.query
-  });
+	let data;
+	if (req.body.eventType === "playerCastSpell") {
+		if (req.body.spellName !== "fulmen" || req.body.spellName !== "ignifera")) {
+			return res.json({ok: true, msg: "Not logging this spell"});
+		} else {
+			data = {
+				spellName: req.body.spellName,
+				playerName: req.body.playerName,
+				eventType: req.body.eventType
+			}
+		}
+	}
+	if (req.body.eventType === "playerKilledEntity") {
+		data = {
+			eventType: req.body.eventType,
+			entityType: victim,
+			playerName: victim.killer
+		}
+	}
+	if (typeof data === "undefined") {
+		return res.json({ok: true, msg: "Not logging this"});
+	}
+	store.push(req.body);
+	console.log('store');
+	console.log(data);
+	res.json({
+		msg: 'Nom nom!',
+		query: data
+	});
 });
 
 // Serves data collected from mock emitter.
 app.get('/store', function (req, res) {
-  res.json({
-    msg: 'Eat Suck Suckface!',
-    data: store
-  });
+	res.json({
+		msg: 'Eat Suck Suckface!',
+		data: store
+	});
 });
 
 // Serves data collected from mock emitter.
 app.get('/stats', function (req, res) {
-  res.json({
-    msg: 'Whos who!',
-    data: getStats()
-  });
+	res.json({
+		msg: 'Whos who!',
+		data: getStats()
+	});
 });
 
 // Clears data from the store.
 app.get('/dumpit', function (req, res) {
-  store = [];
-  res.json({
-    msg: 'Ahhhmazing dump!',
-  });
+	store = [];
+	res.json({
+		msg: 'Ahhhmazing dump!',
+	});
 });
 
 app.use(express.static('app/'));
 
 app.listen(8666, function () {
-  console.log('Example app listening on port 8666!')
+	console.log('Example app listening on port 8666!')
 });
