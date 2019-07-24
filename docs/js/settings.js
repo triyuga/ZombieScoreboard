1fe6a0b7530abb7802309ccb8fe40d3ad3d57358
TIm Marwick <triyuga@gmail.com>
2019-07-24 10:57:13 +1000

    Add /docs dir

---
+++ docs/js/settings.js
@@ -0,0 +1,39 @@
+var defaultInterval = 2000;
+var interval = localStorage.getItem('interval') ? localStorage.getItem('interval') : defaultInterval;
+localStorage.setItem('interval', interval);
+$('#interval').val(interval);
+
+var defaultRestURL = 'http://zombie.magikcraft.io:8666/scoreboard';
+var restURL = localStorage.getItem('restURL') ? localStorage.getItem('restURL') : defaultRestURL;
+localStorage.setItem('restURL', restURL);
+$('#restURL').val(restURL);
+
+var showFeatureGraphics = (localStorage.getItem('showFeatureGraphics') === 'false') ? false : true;
+localStorage.setItem('showFeatureGraphics', showFeatureGraphics);
+$('#showFeatureGraphics').prop('checked', showFeatureGraphics);
+
+var showAzureLogo = (localStorage.getItem('showAzureLogo') === 'false') ? false : true;
+localStorage.setItem('showAzureLogo', showFeatureGraphics);
+$('#showAzureLogo').prop('checked', showAzureLogo);
+
+function saveSettings() {
+  localStorage.setItem('interval', $('#interval').val());
+  localStorage.setItem('restURL', $('#restURL').val());
+  localStorage.setItem('eventTypeJSONs', $('#eventTypeJSONs').val());
+  localStorage.setItem('showFeatureGraphics', $('#showFeatureGraphics').is(":checked"));
+  localStorage.setItem('showAzureLogo', $('#showAzureLogo').is(":checked"));
+  window.location.reload();
+}
+
+function resetSettings() {
+  localStorage.removeItem('interval');
+  localStorage.removeItem('restURL');
+  localStorage.removeItem('eventTypeJSONs');
+  localStorage.removeItem('showFeatureGraphics');
+  localStorage.removeItem('showAzureLogo');
+  window.location.reload();
+}
+
+if (showAzureLogo) {
+  $('.azure-logo').removeClass('hidden');
+}
---
+++ docs/js/zapzap.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $("body").addClass("zapzap");
+    setTimeout(waitForTheZapZap, interval);
+    // $(".feature-graphic").addClass("revealed");
+});
+
+/**
+ *
+ */
+function doTheZapZap() {
+  // doTheZapZap!f
+  // console.log('doTheZapZap');
+  var interval;
+  // 1 in 5 chance of a surprise!
+  var surprise = (Math.floor((Math.random() * 5) + 1) === 5);
+  // susrprise = true;
+  if (surprise) {
+    interval = 1500;
+    setTimeout(waitForTheZapZap, interval);
+    $("body").addClass("faceeater");
+  }
+  else {
+    interval = Math.floor((Math.random() * 5) + 3) * 1000;
+    setTimeout(waitForTheZapZap, interval);
+    $("body").addClass("zapzap");
+
+    // Show random feature graphic, if turned on.
+    var showFeatureGraphics = localStorage.getItem('showFeatureGraphics');1
+    if (showFeatureGraphics !== 'false') {
+      var unhide = Math.floor((Math.random() * 3) + 1);
+      $(".feature-graphic--" + unhide).addClass("revealed");
+    }
+  }
+}
+
+/**
+ *
+ */
+function waitForTheZapZap() {
+  // waitForTheZapZap!
+  // console.log('waitForTheZapZap');
+  var interval = Math.floor((Math.random() * 5) + 3) * 1000;
+  setTimeout(doTheZapZap, interval);
+  $("body").removeClass("zapzap");
+  $("body").removeClass("faceeater");
+  $(".feature-graphic").removeClass("revealed");
+}
